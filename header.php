7853fc6f7ffeff5d2b8c1b20122904a671f801ae
kaushik-tank <91520289+kaushik-tank@users.noreply.github.com>
2021-09-27 23:02:19 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+include "index.html";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+		 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CPT+Serif:400,700,400italic' rel='stylesheet'>
+		  <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+</head>
+<body>
+
+	<div class="bgimage">
+		<div class="menu">
+			
+			<div class="leftmenu">
+				<h4> EMPLOYMENT </h4>
+			</div>
+
+			<div class="rightmenu">
+				<ul>
+					<li id="fisrtlist"><a href="index.html" style="text-decoration: none;color:white;"> HOME </a> </li>
+					<li> <a href="About_us.html" style="text-decoration: none;color:white;">Aboust us</a> </li>
+					<li><a href="contact.html" style="text-decoration: none;color:white;"> contact</a></li>
+					<li><a href="service.html" style="text-decoration: none;color:white;"> Services </a></li>
+					<li><a href="login.html" style="text-decoration: none;color:white;"> Sing In </a></li>
+				</ul>
+			</div>
+
+		</div>
+</div>
+</body>
+</html>
